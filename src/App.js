a65206cc1528efbc39506e19a24c50213ae65f6a
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-17 04:20:16 -0400

    add list

--- src/App.js
+++ src/App.js
@@ -7,7 +7,20 @@ function App() {
       <header>
         <h2 className='headerTitle'>React Course - Project 1</h2>
       </header>
-    <h1>Fun facts about React</h1>
+      <div className='mainBody'>
+        <h1>Fun facts about React</h1>
+
+        <div className = 'bodyText'>
+          <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+          
+        </div>
+      </div>
     </div>
   );
 }
