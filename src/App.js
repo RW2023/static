032070d25239d9b7fb278ef66104312fe68937eb
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-17 11:52:30 -0400

    create JSX functions and render with app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 
 import './App.css';
+const header = (
+  <header>
+    <h2 className='headerTitle'>React Course - Project 1</h2>
+  </header>
+);
+
+const mainBody = (
+  <div className='mainBody'>
+    <h1>Fun facts about React</h1>
+
+    <div className='bodyText'>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+
+    </div>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h2 className='headerTitle'>React Course - Project 1</h2>
-      </header>
-      <div className='mainBody'>
-        <h1>Fun facts about React</h1>
-
-        <div className = 'bodyText'>
-          <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-          </ul>
-          
-        </div>
-      </div>
+      {header}
+      {mainBody}
     </div>
   );
 }
